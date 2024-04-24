8643f746ab88c5148ea03d59da227c6452f5cd53
Ryan Khetlyr <ryan_khetlyr@waed.uscourts.gov>
2024-04-23 21:15:23 -0700

    return custom error

--- rust-bp-error-handling/src/main.rs
+++ rust-bp-error-handling/src/main.rs
@@ -2,7 +2,7 @@ pub type Result<T> = core::result::Result<T,Error>;
 pub type Error = Box<dyn std::error::Error>;
 
 fn main() -> Result<()> {
-    let files = list_files(".")?;
+    let files = list_files("../..")?;
 
     println!("{files:#?}");
 
@@ -10,11 +10,13 @@ fn main() -> Result<()> {
 }
 
 fn list_files(path: &str) -> Result<Vec<String>> {
-    let files: Vec<String> = std::fs::read_dir(path)
-        .map_err(|ex| format!("error while reading dir: {ex}"))?
+    let files: Vec<String> = std::fs::read_dir(path)?
         .filter_map(|re| re.ok())
         .filter(|e| e.file_type().map(|ft| ft.is_file()).unwrap_or(false))
         .filter_map(|e| e.file_name().into_string().ok())
         .collect();
+    if files.is_empty() {
+        return Err("cannot list empty folder ( just for demonstration )".into())
+    }
     Ok(files)
 }
