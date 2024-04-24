87dfa26455cc7a70b239c77a9156b06be6513440
Ryan Khetlyr <ryan_khetlyr@waed.uscourts.gov>
2024-04-23 21:07:36 -0700

    errors passthrough

--- rust-bp-error-handling/src/main.rs
+++ rust-bp-error-handling/src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+pub type Result<T> = core::result::Result<T,Error>;
+pub type Error = Box<dyn std::error::Error>;
+
+fn main() -> Result<()> {
+    let files = list_files("..")?;
+
+    println!("{files:#?}");
+
+    Ok(())
+}
+
+fn list_files(path: &str) -> Result<Vec<String>> {
+    let files: Vec<String> = std::fs::read_dir(path)?
+        .filter_map(|re| re.ok())
+        .filter(|e| e.file_type().map(|ft| ft.is_file()).unwrap_or(false))
+        .filter_map(|e| e.file_name().into_string().ok())
+        .collect();
+    Ok(files)
 }
