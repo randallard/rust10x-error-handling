636b7fa122c4f47a57a0c6232f5a56d2d0639620
Ryan Khetlyr <ryan_khetlyr@waed.uscourts.gov>
2024-04-24 12:07:03 -0700

    with next level production error

--- rust-bp-error-handling/src/main.rs
+++ rust-bp-error-handling/src/main.rs
@@ -6,7 +6,7 @@ pub use self::error::{Error, Result};
 use crate::fs::list_files;
 
 fn main() -> Result<()> {
-    let files = list_files("../..")?;
+    let files = list_files("..")?;
 
     println!("{files:#?}");
 
