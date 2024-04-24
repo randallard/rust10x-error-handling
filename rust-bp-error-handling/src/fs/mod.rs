2f8b85b6035f1ab602a50b30352e30f048ea9dd8
Ryan Khetlyr <ryan_khetlyr@waed.uscourts.gov>
2024-04-23 21:18:51 -0700

    move list_files to module

---
+++ rust-bp-error-handling/src/fs/mod.rs
@@ -0,0 +1,13 @@
+use crate::Result;
+
+pub fn list_files(path: &str) -> Result<Vec<String>> {
+    let files: Vec<String> = std::fs::read_dir(path)?
+        .filter_map(|re| re.ok())
+        .filter(|e| e.file_type().map(|ft| ft.is_file()).unwrap_or(false))
+        .filter_map(|e| e.file_name().into_string().ok())
+        .collect();
+    if files.is_empty() {
+        return Err("cannot list empty folder ( just for demonstration )".into())
+    }
+    Ok(files)
+}
--- rust-bp-error-handling/src/main.rs
+++ rust-bp-error-handling/src/main.rs
@@ -1,22 +1,14 @@
 pub type Result<T> = core::result::Result<T,Error>;
 pub type Error = Box<dyn std::error::Error>;
 
+mod fs;
+
+use crate::fs::list_files;
+
 fn main() -> Result<()> {
     let files = list_files("../..")?;
 
     println!("{files:#?}");
 
     Ok(())
-}
-
-fn list_files(path: &str) -> Result<Vec<String>> {
-    let files: Vec<String> = std::fs::read_dir(path)?
-        .filter_map(|re| re.ok())
-        .filter(|e| e.file_type().map(|ft| ft.is_file()).unwrap_or(false))
-        .filter_map(|e| e.file_name().into_string().ok())
-        .collect();
-    if files.is_empty() {
-        return Err("cannot list empty folder ( just for demonstration )".into())
-    }
-    Ok(files)
 }
